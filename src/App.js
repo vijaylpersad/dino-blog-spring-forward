034135d2107592e044adc3be14edb0a30201b822
weston-bailey <wdbailey87@gmail.com>
2022-03-09 09:47:06 -0800

    adds state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import React, { Component } from 'react'
 import Post from './Post'
 import posts from './posts'
 
+
 class App extends Component {
+  state = {
+    body: '',
+    comments: [],
+    author: '',
+    title: ''
+  }
+
+  handleTextChange = (e) => {
+    const updatedInput = { [e.target.name]: e.target.value }
+    this.setState({
+      ...this.state,
+      ...updatedInput
+    })
+    console.log(this.state)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    posts.push(this.state)
+
+    this.setState({
+      body: '',
+      comments: [],
+      author: '',
+      title: ''
+    })
+
+    console.log(posts)
+  }
+
   render() {
     const postComponents = posts.map((post, index) => {
       return (
@@ -16,9 +47,37 @@ class App extends Component {
         />
       )
     })
+
     return(
       <>
         {postComponents}
+
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor="author">Name:</label>
+        <input 
+          name="author" 
+          id="author"
+          onChange={this.handleTextChange} 
+        />
+
+        <label htmlFor="title">Post Title:</label>
+        <input 
+          name="title" 
+          id="title" 
+          onChange={this.handleTextChange} 
+        />
+
+        <label htmlFor="body">Post:</label>
+        <textarea 
+          id="body"
+          name='body'
+          onChange={this.handleTextChange}
+          value={this.state.body}
+        >
+        </textarea>
+
+        <input type="submit" />
+      </form>
       </>
     )
   }
